3d07a52a4a74ac115a19957cfe23381cf689d315
kiranpratyush <pratyushkiranrout@gmail.com>
2021-07-23 15:32:18 +0530

    done exercise to read the value of text Area

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello word");
+'use strict'
+let btntranslate =document.querySelector("#translate");
+let textAreaValue = document.querySelector("#translator-textarea");
+
+function click()
+{
+    let value = textAreaValue.value;
+    console.log(value);
+}
+translate.addEventListener("click",click);
